0aa157090d2cfb2eca26087cac6ceb89c9b5b14c
viclm <viclm@live.com>
2015-10-08 17:05:39 +0800

    Initial Commit

---
+++ Hosts/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hosts
+//
+//  Created by 刘明 on 15/10/8.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hosts/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hosts
+//
+//  Created by 刘明 on 15/10/8.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
