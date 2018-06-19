02c283892fd99c5d711fea715650134230ca5f02
viclm <xliuming@outlook.com>
2018-06-19 18:04:52 +0800

    update toolbar icon

--- Hosts/AppDelegate.swift
+++ Hosts/AppDelegate.swift
@@ -11,14 +11,16 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
   
-  let statusItem = NSStatusBar.system.statusItem(withLength: -1)
+  let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
   
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     // Insert code here to initialize your application
-    let icon = NSImage(named: "AppIcon")
-    icon!.size = NSSize(width: 22, height: 22)
-    statusItem.image = icon
-    statusItem.button!.action = #selector(AppDelegate.activateApplication)
+    if let button = statusItem.button {
+      let icon = NSImage(named:NSImage.Name("AppIcon"))
+      icon?.size = NSSize(width: 22, height: 22)
+      button.image = icon
+      button.action = #selector(activateApplication)
+    }
   }
   
   func applicationWillTerminate(_ aNotification: Notification) {
